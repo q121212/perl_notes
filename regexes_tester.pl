4d3348f5f44b026d93b26e9725ba912239afb5ff
Max R <q121212@gmail.com>
2017-01-28 01:46:14 +0300

    Moved the regexes_tester code to appropriate file

---
+++ regexes_tester.pl
@@ -0,0 +1,51 @@
+#!/usr/bin/perl
+
+use strict;
+use warnings;
+
+sub regex_matches ($){
+	my $pattern = $_[0];
+	my $result;
+	print "Input string for regex_tester:\n";
+	while (<STDIN>) {
+	chomp;
+	if (/$pattern/){
+		$result = "\tMatches\n";
+		}
+	else {
+		$result = "\tDoesn't match\n"
+		}
+	print($result);
+	logging($pattern, $_, $result);
+	}
+}
+
+sub regex_tester{
+	my $pattern;
+	if (!@ARGV){
+		print "Input pattern, please, and press ^D:\n";
+		while (<STDIN>) {
+			chomp;
+			$pattern = $_;
+		}
+		regex_matches($pattern);
+		}
+	else{
+		$pattern = join ' ', @ARGV;
+		regex_matches($pattern);
+	}
+}
+
+
+sub logging($$$){
+	my ($log_regex_pattern, $log_regex_expression, $result) = @_; 
+	# my ($sec,$min,$hour,$mday,$mon,$year,$wday,$yday,$isdst) = localtime();
+	open F, '>>', 'regex_tester.log' || die "Can't open file: $!";
+	print F "log_regex_pattern:\t". $log_regex_pattern. "\n";
+	print F "log_regex_expression:\t". $log_regex_expression. "\n";
+	print F "Result:\t". $result;
+	print F "-------- ".`date +"%c"`."\n";
+	close(F);
+}
+
+regex_tester;
